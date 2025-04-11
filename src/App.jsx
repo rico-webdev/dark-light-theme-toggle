a27d2ebf57cc56da2455bcc07a895c3728886d9a
ricotech.dev <151479600+rico-webdev@users.noreply.github.com>
2025-04-11 19:21:58 +0200

    create background-gradient for light theme

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,28 @@ const App = () => {
   }, [isDarkMode]);
 
   return (
-    <div className="min-h-screen dark:bg-neutral-950 bg-neutral-100 relative transition-colors duration-300">
+    <div className="min-h-screen dark:bg-neutral-950 bg-neutral-100 relative transition-colors duration-300 isolate">
+      <div className="absolute inset-0 -z-10 isolate">
+        <div
+          className="absolute inset-0 opacity-30 dark:hidden"
+          style={{
+            backgroundImage: `
+        linear-gradient(to right, rgba(0, 0, 0, 0.05) 1px, transparent 1px),
+        linear-gradient(to bottom, rgba(0, 0, 0, 0.05) 1px, transparent 1px)
+      `,
+            backgroundSize: "40px 40px",
+          }}
+        ></div>
+        <div
+          className="absolute inset-0 dark:hidden"
+          style={{
+            backgroundImage:
+              "radial-gradient(rgba(0,0,0, 0.05) 1px, transparent 1px)",
+            backgroundSize: "20px 20px",
+          }}
+        ></div>
+      </div>
+
       <button
         onClick={toggleDarkMode}
         className="fixed top-3 lg:top-4 right-3 lg:right-4 w-9 h-9 lg:w-10 lg:h-10 flex justify-center items-center rounded-full bg-amber-500 text-neutral-950 shadow-lg hover:bg-amber-600 transition-colors cursor-pointer"
